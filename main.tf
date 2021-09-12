c77486ef56687d16859339089b023c4f6f1ebfba
Aliu Raimi Olatunde <olatunderaimialiu@gmail.com>
2021-09-12 15:56:51 +0100

    wanted to add sql db

--- main.tf
+++ main.tf
@@ -12,12 +12,28 @@ variable "default_location" {
 locals {
   rg_name = var.rg_name
   rg_location = var.rg_location
+  db_location ="armtuts"
 }
 
+data "azurerm_sql_server" "kangroodb" {
+    name = "kangarooaccount"
+    resource_group_name = local.db_location
+  
+}
 
 resource "azurerm_resource_group" "terraform_training" {
   name = var.rg_name
   location = var.rg_location
+}
+
+
+resource "azurerm_sql_database" "kangarooaccountdb" {
+  resource_group_name = local.db_location
+  server_name = data.azurerm_sql_server.kangroodb.name
+  name = "kangaroosecondb"
+  tags = {
+    "purpose" = "terraform training"
+  }
 }
 
 output "rg_name" {
