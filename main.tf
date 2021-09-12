8a2b67e2e0e05e243e32ec653a0e7c3ba148a4e1
Aliu Raimi Olatunde <olatunderaimialiu@gmail.com>
2021-09-12 09:41:18 +0100

    added output value

--- main.tf
+++ main.tf
@@ -15,14 +15,19 @@ variable "rg_name" {
 
 variable "rg_location" {
   description = "default resource group location"
-  validation {
-    condition = can(index(["West US", "East US"],var.location) >= 0)
-    error_message ="the location must be east or west us"
-  }
+#   validation {
+#     condition = can(index(["West US", "East US"],var.location) >= 0)
+#     error_message ="the location must be east or west us"
+#   }
 }
 
 
 resource "azurerm_resource_group" "terraform_training" {
   name = var.rg_name
   location = var.rg_location
+}
+
+output "rg_name" {
+  value = azurerm_resource_group.terraform_training
+  description = "output of resource group"
 }
